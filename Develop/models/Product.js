8eeb8a215b51c282626542d353a6cb51a41c3975
Charlie <charlieboyle252@gmail.com>
2024-07-19 17:34:56 -0500

    completion of sequelize models

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -3,10 +3,9 @@ const { Model, DataTypes } = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
 
-// Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
 
-// set up fields and rules for Product model
+// Fields and rules for Product model
 Product.init(
   {
     id: {
@@ -22,13 +21,25 @@ Product.init(
     price: {
       type: DataTypes.DECIMAL,
       allowNull: true,
-      
+      isDecimal: true,
+    },
 
-    }
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      defaultValue: 10,
+      isNumeric: true,
+    },
 
-
-
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Category',
+        key: 'id',
+      },
+    },
   },
+
   {
     sequelize,
     timestamps: false,
--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -6,7 +6,29 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Product',
+        key: 'id',
+      },
+    },
+
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Tag',
+        key: 'id',
+      },
+    },
+
   },
   {
     sequelize,
@@ -15,6 +37,8 @@ ProductTag.init(
     underscored: true,
     modelName: 'product_tag',
   }
+
+  
 );
 
 module.exports = ProductTag;
